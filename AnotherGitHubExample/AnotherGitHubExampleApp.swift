5ad7dca122051ef7a9b76a9c7365a0f1b6b285a9
martabella <mbella@lasalle.cat>
2024-04-15 13:18:13 +0200

    Initial Commit

---
+++ AnotherGitHubExample/AnotherGitHubExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnotherGitHubExampleApp.swift
+//  AnotherGitHubExample
+//
+//  Created by LSOnline on 15/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct AnotherGitHubExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
